f9a47a597ba80853d0f168bf01dfa33f287e8547
Lorenzo Vaquero <lorenzovaquero@lorenzos-mbp.fbkeduroam.it>
2024-09-05 10:55:20 +0200

    Initial commit

---
+++ get_freq.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import requests
+
+def call_api(query_text):
+    base_url = "https://api.ngrams.dev"
+    corpus = "eng"
+    endpoint = "search"
+
+    extra_params = "flags=cr+cs"
+    
+    api_url = "{}/{}/{}?query={}&{}".format(base_url, corpus, endpoint, query_text, extra_params)
+    response = requests.get(api_url)
+
+    return response.json()
+
+def querify(text, join_with_star=False):
+    # Takes a text and converts it to a query string
+    # We take into account variatons of the text (right now with ~, but we need to do something more elaborate)
+    
+    verb = text[0]
+    obj = text[1]
+
+    # Verb can have a preposition, so we only augment the first word
+    verb_root = verb.split(' ')[0].strip()
+    verb_root = verb_root + '~'  # We augment the verb.  # TODO: We need to do something more elaborate here (like ask GPT)
+
+    verb_preposition = verb.split(' ')[1:]
+    verb_preposition = [p.strip() for p in verb_preposition]
+
+    verb_query = [verb_root]
+    verb_query.extend(verb_preposition)
+    verb_query = '+'.join(verb_query)
+
+    # For object, we augment all words
+    obj = obj.split(' ')
+    obj = [o.strip() + '~' for o in obj]  # We augment the object.  # TODO: We need to do something more elaborate here (like ask GPT for plurals)
+    obj_query = '+'.join(obj)
+
+    if join_with_star:
+        query = "{}+*+{}".format(verb_query, obj_query)  # We add * to account for articles, adjectives, etc.
+    else:
+        query = "{}+{}".format(verb_query, obj_query)
+
+    return query
+
+def aggregate_freq(api_response):
+    # Response looks like this:
+    ngrams = api_response['ngrams']
+    total_freq = 0
+    
+    for ngram in ngrams:
+        total_freq += ngram['absTotalMatchCount']
+    
+    return total_freq
+
+
+
+def get_frequency(text):
+    text_query = querify(text)
+    text_query_wildcard = querify(text, join_with_star=True)
+    
+    api_response = call_api(text_query)
+    api_response_wildcard = call_api(text_query_wildcard)
+    
+    freq = aggregate_freq(api_response)
+    freq_wildcard = aggregate_freq(api_response_wildcard)
+
+    final_freq = max(freq, freq_wildcard)
+
+    return final_freq
+
+
+
+def process_file(file_path):
+    actions = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            action = line.split(',')
+            action = [action[0].strip(), action[1].strip()]  # Verb and object
+            actions.append(action)
+    return actions
+
+def process_text(text):
+    text = text.strip()
+    action = text.split(',')
+    action = [action[0].strip(), action[1].strip()]  # Verb and object
+    actions = [action]
+    return actions
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Retrieve HOI frequencies')
+    parser.add_argument('--file', help='Path to the input file')
+    parser.add_argument('--text', help='Input text')
+
+    args = parser.parse_args()
+
+    if args.file:
+        actions = process_file(args.file)
+    elif args.text:
+        actions = process_text(args.text)
+    else:
+        print("Please provide either a file or text as input.")
+        raise SystemExit
+    
+    for action in actions:
+        action_freq = get_frequency(action)
+        print('{}: {}'.format(action, action_freq))
+    
